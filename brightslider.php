9d161c09f40610764e13a069505a3afd94f624bf
Galen Gidman <galen@galengidman.com>
2014-10-17 12:51:08 -0500

    include plugin dir in includes

--- brightslider.php
+++ brightslider.php
@@ -14,24 +14,29 @@ Domain Path: /languages
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 /**
+ * Store the plugins dir.
+ */
+$plugin_dir = dirname( __FILE__ );
+
+/**
+ * Load includes.
+ */
+require_once $plugin_dir . '/includes/post-types.php';
+
+if ( is_admin() ) {
+  require_once $plugin_dir . '/includes/admin/admin-support.php';
+  require_once $plugin_dir . '/includes/admin/slide-fields.php';
+  require_once $plugin_dir . '/includes/library/ct-meta-box/ct-meta-box.php';
+}
+
+/**
  * Load text domain.
  */
 function brightslider_load_plugin_textdomain() {
 
-  $lang_dir = basename( dirname( __FILE__ ) ) . '/languages/';
+  $lang_dir = $plugin_dir . '/languages/';
 
   load_plugin_textdomain( 'brightslider', FALSE, $lang_dir );
 
 }
 add_action( 'plugins_loaded', 'brightslider_load_plugin_textdomain' );
-
-/**
- * Load includes.
- */
-require_once 'includes/post-types.php';
-
-if ( is_admin() ) {
-  require_once 'includes/admin/admin-support.php';
-  require_once 'includes/admin/slide-fields.php';
-  require_once 'includes/library/ct-meta-box/ct-meta-box.php';
-}
